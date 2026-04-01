68d02467e37235c05c68a0d5edf8ecd3e9309c7d
mehakmehak221 <mehak.gaur@maxtron.ai>
2026-04-01 18:15:30 +0530

    feat: url changed in env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = (process.env.NEXT_PUBLIC_API_URL || "https://glofi-api.maxtron.ai").replace(/\/$/, "");
+export const API_URL = (process.env.NEXT_PUBLIC_API_URL || "https://api.glofiestate.com").replace(/\/$/, "");
